4d9d6d93611095365b5f2d57aea3430bf15d51fd
Enzo Seiti Hashimoto <enzoseiti@gmail.com>
2025-05-08 20:23:44 -0300

    Add files via upload

---
+++ App - Copia.vue
@@ -0,0 +1,27 @@
+<script>
+  export default{
+  data(){
+    return {
+      nome: "",
+      senha: "",
+      confsenha: "",
+    };
+  },
+};
+
+</script>
+
+<template>
+  <label>Login</label>
+  <input v-model="nome"/>
+
+  <label>senha</label>
+  <input v-model="senha"/>
+
+  <label>confirmar senha</label>
+  <input v-model="confsenha"/>
+
+  <h1 v-if="senha != confsenha">Senha Incorreta</h1>
+</template>
+
+<style></style>
---
+++ App.vue
@@ -0,0 +1,35 @@
+<script>
+  export default{
+  data(){
+    return {
+      pessoas:[
+        {nome: 'Hugo', idade: 20},
+        {nome: 'Heitor', idade: 30},
+        {nome: 'Luiz', idade: 40},
+        {nome: 'Pablo', idade: 50},
+      ]
+    };
+  },
+};
+
+</script>
+
+<template>
+  <table class="table">
+    <thead>
+      <tr>
+        <th>Nome</th>
+        <th>Idade</th>
+      </tr>
+    </thead>
+
+    <tbody>
+      <tr v-for="pessoa in pessoas">
+        <th>{{ pessoa.nome }}</th>
+        <th>{{ pessoa.idade }}</th>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<style></style>
